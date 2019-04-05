905ce9143d9f78011343ad1dec077d1af45f78df
Norris <norris.wu.au@outlook.com>
2019-04-05 19:24:47 +1100

    Initial commit

---
+++ Assets/Scripts/LightFlash.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightFlash : MonoBehaviour
+{
+    [SerializeField] private float delay = 1.0f;
+    public GameObject[] lights;
+
+    private void Start()
+    {
+        lights = GameObject.FindGameObjectsWithTag("Light");
+        while (true)
+            StartCoroutine(Flash());
+    }
+    
+    IEnumerator Flash()
+    {
+        yield return new WaitForSeconds(delay);
+
+        foreach (GameObject light in lights)
+        {
+            light.SetActive(!light.activeSelf);
+            light.SetActive(!light.activeSelf);
+        }
+
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Boundary{
+    public float xMin, xMax, zMin, zMax;
+}
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private MeshCollider collider;
+    [SerializeField] private float speed;
+    [SerializeField] private Vector2 tilt;
+    [SerializeField] private Boundary boundary;
+
+    private Rigidbody rb;
+    private float moveHorizontal, moveVertical;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        moveHorizontal = Input.GetAxis("Horizontal");
+        moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0, moveVertical);
+
+        rb.velocity = movement * speed;
+        rb.rotation = Quaternion.Euler(moveVertical * tilt.x, 0, moveHorizontal * -tilt.y);
+        rb.position = new Vector3(
+            Mathf.Clamp(rb.position.x, boundary.xMin, boundary.xMax),
+            0.0f,
+            Mathf.Clamp(rb.position.z, boundary.zMin, boundary.zMax)
+        );
+    }
+}
